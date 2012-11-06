1bdbd1f3a0ca0a58054bf662628f3ea71c005a83
Christian Brandenstein <cbr@adorsys.de>
2012-11-06 19:13:13 +0100

    lic

--- src/main/java/de/adorsys/beanstest/plugin/cdi/CDITestFacet.java
+++ src/main/java/de/adorsys/beanstest/plugin/cdi/CDITestFacet.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2012 Christian Brandenstein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.adorsys.beanstest.plugin.cdi;
 
 import java.io.File;
@@ -54,13 +69,13 @@ public class CDITestFacet extends BaseFacet {
 					+ descriptor.getFullyQualifiedName() + "]");
 		}
 		descriptor.setContents(getClass().getResourceAsStream(
-				"/de/adorsys/cditest/beans.xml"));
+				"/de/adorsys/beanstest/beans.xml"));
 
 		// create Runner
 		final JavaSourceFacet java = project.getFacet(JavaSourceFacet.class);
 
 		JavaClass javaResource = JavaParser.parse(JavaClass.class, getClass()
-				.getResourceAsStream("/de/adorsys/cditest/SimpleRunner.jv"));
+				.getResourceAsStream("/de/adorsys/beanstest/SimpleRunner.jv"));
 		javaResource.setPackage(java.getBasePackage());
 
 		try {
